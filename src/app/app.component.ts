ba42bd6b2b61b620dea2ce53380a9b3d1153600f
Gil Steiner <games.gil@gmail.com>
2024-06-12 20:44:28 +0300

    game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+ clicked: number[] = [];
+
   title = 'ttt';
+
+  onClick(idx: number) {
+    if (!this.isClicked(idx)) {
+      this.clicked.push(idx)
+    }
+
+    console.log(this.clicked)
+    }
+
+    isClicked(idx: number) {
+      return this.clicked.indexOf(idx) > -1
+    }
+
+    isWinner(idx: number) {
+      return  (this.clicked.indexOf(idx) === 3);
+      }
+       
 }
